68e3ec332e1622574cbd391d1cb6215219b32363
Anthony Navarro <anavarroa@three29.com>
2017-10-27 09:24:53 -0700

    Add some more interactiveness

--- javascript/controllers/rens_thoughts/thinking.js
+++ javascript/controllers/rens_thoughts/thinking.js
@@ -2,12 +2,36 @@ class Thinking {
 
     process_request( input )
     {
-        var my_response = this.response_type( input, this.regex_list() );
-
+        var query_type = this.response_type( input, this.regex_list_request_type() );
+        var my_response = '';
+console.log('going to parse query_type: ' + query_type);
+        // filter to query type
+        switch( query_type )
+        {
+            case 'question':
+                my_response = this.response_type( input, this.regex_list_question_responses() );
+                break;
+            case 'statement':
+                my_response = this.response_type( input, this.regex_list_statement_responses() );
+                break
+            default:
+                return this.i_dont_understand();
+                break;
+        }
+console.log('going to parse my_response: ' + my_response);
+        // filter to reply
         switch( my_response )
         {
+            // Statements
             case 'greeting':
                 return this.greeting();
+                break;
+            // Questions
+            case 'my_name':
+                return this.my_name();
+                break;
+            case 'i_dont_know':
+                return this.i_dont_know();
                 break;
             default:
                 return this.i_dont_understand();
@@ -15,11 +39,30 @@ class Thinking {
         }
     }
 
-    regex_list()
+    regex_list_request_type()
     {
         return {
-            'greeting' : new RegExp(/(^([H])(i|ey|ello)|^[Y]o$)/i)
+            'question' : new RegExp( /^[w](ill|hat|here|hy)|^[h](ow)|^[a](re)/i ),
+            'statement' : new RegExp( /\.*$/i )
         };
+    }
+
+    regex_list_statement_responses()
+    {
+        return {
+            'greeting' : new RegExp( /(^[H](i|ey|ello)|^[Y]o)/i )
+        };
+    }
+
+    regex_list_question_responses()
+    {
+        return {
+            'my_name' : new RegExp(/[n](ame)/i),
+            'i_dont_know' : new RegExp( /(\?)*$/i )
+        };
+        // return {
+        //     '' : 
+        // };
     }
 
     response_type( query, arr )
@@ -43,6 +86,16 @@ class Thinking {
         {
             return 'Hi, stranger!';
         }
+    }
+
+    my_name()
+    {
+        return 'My name is Ren!';
+    }
+
+    i_dont_know()
+    {
+        return 'I don\'t know how to answer that question...';
     }
 
     i_dont_understand()
